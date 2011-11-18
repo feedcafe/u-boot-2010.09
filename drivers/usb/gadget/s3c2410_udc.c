c4acd517676e5285f379e3fc568214a6b9a4f2fe
Fudong Bai <fudongbai@gmail.com>
2011-11-18 20:34:06 +0800

    udc: typo
    Signed-off-by: Fudong Bai <fudongbai@gmail.com>

--- drivers/usb/gadget/s3c2410_udc.c
+++ drivers/usb/gadget/s3c2410_udc.c
@@ -417,7 +417,7 @@ static void s3c2410_udc_epn(int ep)
 			if (fifo_count < endpoint->rcv_packetSize)
 				is_last = 1;
 
-			debug("fifo_count=%u is_last=%, urb_avail=%u\n",
+			debug("fifo_count=%u is_last=d%, urb_avail=%u\n",
 				fifo_count, is_last, urb_avail);
 
 			if (fifo_count < urb_avail)
