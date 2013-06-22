d8b1ad2eaf61432173c20e496ffe1a8daa568f80
Fudong Bai <fudongbai@gmail.com>
2013-06-22 10:44:33 +0800

    nand_read_ll: auto detect nand page size
    This is not a complete implementation, only support 2k and 512 page
    size, this commit is based on the following information from S3C2440
    datasheet:
    
    NCON: NAND flash memory selection(Normal / Advance)
    0: Normal NAND flash(256Words/512Bytes page size, 3/4 address cycle)
    1: Advance NAND flash(1KWords/2KBytes page size, 4/5 address cycle)
    
    GPG13: NAND flash memory page capacitance selection
    0: Page=256Words(NCON = 0) or Page=1KWords(NCON = 1)
    1: Page=512Bytes(NCON = 0) or Page=2KBytes(NCON = 1)
    
    GPG14: NAND flash memory address cycle selection
    0: 3 address cycle(NCON = 0) or 4 address cycle(NCON = 1)
    1: 4 address cycle(NCON = 0) or 5 address cycle(NCON = 1)
    
    GPG15: NAND flash memory bus width selection
    0: 8-bit bus width
    1: 16-bit bus width
    
    Signed-off-by: Fudong Bai <fudongbai@gmail.com>

--- board/samsung/mini2440/nand_read.c
+++ board/samsung/mini2440/nand_read.c
@@ -22,7 +22,8 @@
  *
  */
 
-#include <config.h>
+#include <common.h>
+#include <asm/arch/s3c24x0_cpu.h>
 
 #define __REGb(x)	(*(volatile unsigned char *)(x))
 #define __REGi(x)	(*(volatile unsigned int *)(x))
@@ -49,45 +50,89 @@ inline void wait_idle(void) {
 		for(i=0; i<10; i++);
 }
 
-#define NAND_SECTOR_SIZE	512
-#define NAND_BLOCK_MASK		(NAND_SECTOR_SIZE - 1)
+static void s3c2440_write_addr_page_2k(unsigned addr, unsigned mask, unsigned page_size)
+{
+	int col, page;
+
+	col = addr & mask;
+	page = addr / page_size;
+
+	NFADDR = col & 0xff;		/* Column Address A0~A7 */
+	NFADDR = (col >> 8) & 0x0f;	/* Column Address A8~A11 */
+	NFADDR = page & 0xff;		/* Row Address A12~A19 */
+	NFADDR = (page >> 8) & 0xff;	/* Row Address A20~A27 */
+	NFADDR = (page >> 16) & 0x01;	/* Row Address A28 */
+}
+
+static void s3c2440_write_addr_page_512(unsigned int addr)
+{
+	/* Write Address */
+	NFADDR = addr & 0xff;
+	NFADDR = (addr >> 9) & 0xff;
+	NFADDR = (addr >> 17) & 0xff;
+	NFADDR = (addr >> 25) & 0xff;
+}
 
 /* low level nand read function */
-	int
-nand_read_ll(unsigned char *buf, unsigned long start_addr, int size)
+int nand_read_ll(unsigned char *buf, unsigned long start_addr, int size)
 {
 	int i, j;
+	struct s3c24x0_gpio * const gpio = s3c24x0_get_base_gpio();
 
-	if ((start_addr & NAND_BLOCK_MASK) || (size & NAND_BLOCK_MASK)) {
+	unsigned char ncon = (gpio->GSTATUS0 & 0x4) >> 2;
+	unsigned char gpg13 = (gpio->GPGDAT & 0x2000) >> 13;
+	unsigned page_size;
+	unsigned nand_block_mask;
+
+	if (ncon == 1)
+		if (gpg13 == 1)
+			page_size = 2048;
+		else
+			page_size = 1024;
+	else
+		if (gpg13 == 1)
+			page_size = 512;
+		else
+			page_size = 256;
+
+	nand_block_mask = page_size - 1;
+
+	if ((start_addr & nand_block_mask) || (size & nand_block_mask)) {
 		return -1;	/* invalid alignment */
 	}
 
+	NFCONF = (3 << 8);
 	/* chip Enable */
 	NFCONT &= ~(1<<1);
-	for(i=0; i<10; i++);
+	for (i = 0; i < 10; i++);
 
-	for(i=start_addr; i < (start_addr + size);) {
+	for (i = start_addr; i < (start_addr + size);) {
 		/* READ0 */
 		NFSTAT |= (1<<2);
 		NFCMD = 0;
 
-		/* Write Address */
-		NFADDR = i & 0xff;
-		NFADDR = (i >> 9) & 0xff;
-		NFADDR = (i >> 17) & 0xff;
-		NFADDR = (i >> 25) & 0xff;
+		if (page_size == 2048) {
+			s3c2440_write_addr_page_2k(i, nand_block_mask, page_size);
+			NFCMD = 0x30;
+		} else if (page_size == 1024) {
+			/* TODO */
+		} else if (page_size == 512) {
+			s3c2440_write_addr_page_512(i);
+		} else if (page_size == 256) {
+			/* TODO */
+		}
 
 		//wait_idle();
 		NAND_DETECT_RB;
 
-		for(j=0; j < NAND_SECTOR_SIZE; j++, i++) {
+		for(j = 0; j < page_size; j++, i++) {
 			*buf = (NFDATA & 0xff);
 			buf++;
 		}
 	}
 
 	/* chip Disable */
-	NFCONF |= 0x800;	/* chip disable */
+	NFCONT |= (1 << 1);	/* chip disable */
 
 	return 0;
 }
